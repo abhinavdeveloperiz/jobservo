15c965f63b739dc4e5fed24142e403ef6791f0d1
abhinavmabhi <dexmarketing192018@gmail.com>
2026-04-04 16:12:57 +0530

    added speed from vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AOS from "aos";
 import { useEffect } from "react";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import ScrollToTop from "./components/ScrollToTop";
 
@@ -48,6 +49,7 @@ function App() {
         {/* Bottom Navbar */}
         <Navbar />
         <Analytics />
+        <SpeedInsights />
       </div>
     </Router>
   );
